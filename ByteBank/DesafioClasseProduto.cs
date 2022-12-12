08bc063e91680c5ba702841aa827ef27902a7616
Felipe ALCM <felipealcmagalhaes@gmail.com>
2022-12-12 14:07:17 -0300

    Criacao da Classe ContaCorrente e metodos iniciais

---
+++ ByteBank/DesafioClasseProduto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    internal class DesafioClasseProduto
+    {
+        public string nome;
+        public string fabricante;
+        public long codigo;
+        public double preco;
+        public double valorDeCompra;
+        public int quantidade;
+
+    }
+}
---
+++ ByteBank/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ByteBank
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ContaCorrente contaDoAndre = new ContaCorrente();
+            contaDoAndre.titular = "André Silva";
+            contaDoAndre.agencia = 15;
+            contaDoAndre.conta = "1010-X";
+            contaDoAndre.saldo = 100;
+
+            
+
+            Console.WriteLine(contaDoAndre.titular);
+            Console.WriteLine(contaDoAndre.agencia);
+            Console.WriteLine(contaDoAndre.conta);
+            Console.WriteLine(contaDoAndre.saldo);
+            contaDoAndre.Depositar(100);
+            Console.WriteLine($"Saldo pós deposito {contaDoAndre.saldo}");
+
+            if(contaDoAndre.Sacar(300)== true)
+            {
+                Console.WriteLine($"Saldo pós saque {contaDoAndre.saldo}");
+
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente para saque");
+            }
+            
+        }
+    }
+}
